d6a3af1b22a148a2186b25bd2ea51ca4698f159a
manojprabaker <manojprabaker999@gmail.com>
2023-03-02 12:51:05 +0530

    add

---
+++ main.js
@@ -0,0 +1,26 @@
+var a=["a","b","c","d","e"];
+function reverse(a){
+	  for(i=a.length-1; i>=0; i--)
+	  {
+		  console.log(a[i]);
+	  }
+}
+reverse(a); 
+ 
+var arr=[1,2,3,4,5,6];
+var total=0;
+function sum(arr){
+    for (i=0; i<arr.length; i++)
+    {
+        total=total + arr[i];
+    }
+	console.log(total);
+	
+}
+sum(arr);
+
+var a1=[1,2,3,4,5];
+for(i=0;i<=0;i--){
+   i=a1.length-1;
+console.log(a1[i]);   
+}
